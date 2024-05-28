aa1426f9b87d660a29c33a8a7928304f2362e347
igalklebanov <igalklebanov@gmail.com>
2024-05-28 13:06:30 +0300

    bump bun example

--- examples/bun/.config/kysely.config.ts
+++ examples/bun/.config/kysely.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "kysely-ctl";
+// @ts-ignore no idea why it can't find the module despite @types/bun being installed
 import { Database } from "bun:sqlite";
 import { BunSqliteDialect } from "kysely-bun-sqlite";
 
