531fc5699ff61f9d90e7aab8823cc8dcb23dd69c
igalklebanov <igalklebanov@gmail.com>
2024-07-04 03:12:39 +0300

    auto|off|config.

--- src/templates/seed-type-safe-template.ts
+++ src/templates/seed-type-safe-template.ts
@@ -1,7 +1,7 @@
-import type { <typename> } from '<path>'
 import type { Kysely } from 'kysely'
+<import>
 
-export async function seed(db: Kysely<<typename>>): Promise<void> {
+export async function seed(db: Kysely<<name>>): Promise<void> {
 	// seed code goes here...
 	// note: this function is mandatory. you must implement this function.
 }
