0002f913b43ee66fe03e6a18662494783d73dd71
igalklebanov <igalklebanov@gmail.com>
2024-12-20 00:11:15 +0200

    format.

--- examples/deno-package-json/migrations/1716743937856_add_table_moshe.ts
+++ examples/deno-package-json/migrations/1716743937856_add_table_moshe.ts
@@ -1,9 +1,9 @@
-import type { Kysely } from "kysely";
+import type { Kysely } from 'kysely'
 
 export async function up(db: Kysely<any>): Promise<void> {
-  await db.schema.createTable("moshe").addColumn("id", "integer").execute();
+	await db.schema.createTable('moshe').addColumn('id', 'integer').execute()
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
-  await db.schema.dropTable("moshe").execute();
+	await db.schema.dropTable('moshe').execute()
 }
--- examples/node-esm/.config/kysely.config.ts
+++ examples/node-esm/.config/kysely.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "kysely-ctl";
-import database from "better-sqlite3";
+import { defineConfig } from 'kysely-ctl'
+import database from 'better-sqlite3'
 
 export default defineConfig({
-  dialect: "better-sqlite3",
-  dialectConfig: {
-    database: database("./example.db"),
-  },
-});
+	dialect: 'better-sqlite3',
+	dialectConfig: {
+		database: database('./example.db'),
+	},
+})
