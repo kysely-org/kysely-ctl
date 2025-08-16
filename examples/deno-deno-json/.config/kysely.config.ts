4d7618c78982ddb7fca05601434c9084a0f755b7
igalklebanov <igalklebanov@gmail.com>
2025-08-17 00:13:34 +0300

    ...

--- examples/deno-deno-json/.config/kysely.config.ts
+++ examples/deno-deno-json/.config/kysely.config.ts
@@ -1,14 +1,12 @@
+import { resolve } from 'node:path'
 import { Database } from '@jsr/db__sqlite'
 import { DenoSqlite3Dialect } from '@jsr/soapbox__kysely-deno-sqlite'
-import { dirname, fromFileUrl, resolve } from '@std/path'
 import { defineConfig } from 'kysely-ctl'
 
 if (Deno.env) {
 	// biome-ignore lint/suspicious/noConsole: console.log is fine here
 	console.log('Deno env is available')
 }
-
-const __dirname = dirname(fromFileUrl(import.meta.url))
 
 export default defineConfig({
 	dialect: new DenoSqlite3Dialect({
