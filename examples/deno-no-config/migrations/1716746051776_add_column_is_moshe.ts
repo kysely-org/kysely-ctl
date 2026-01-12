35ac645c012555dc3184245922459ccf1bf9a035
igalklebanov <igalklebanov@gmail.com>
2026-01-12 03:54:16 +0200

    chore: remove no config deno example.

--- examples/deno-no-config/migrations/1716746051776_add_column_is_moshe.ts
+++
@@ -1,9 +0,0 @@
-import type { Kysely } from 'npm:kysely@^0.28.9'
-
-export async function up(db: Kysely<any>): Promise<void> {
-	await db.schema.alterTable('moshe').addColumn('is_moshe', 'boolean').execute()
-}
-
-export async function down(db: Kysely<any>): Promise<void> {
-	await db.schema.alterTable('moshe').dropColumn('is_moshe').execute()
-}
